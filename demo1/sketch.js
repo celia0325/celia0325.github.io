88b1c4d01f2406dbd9e8a793abb8089d93274c8f
celia0325 <celiajnewell@gmail.com>
2022-10-03 11:49:57 -0600

    demo

---
+++ demo1/sketch.js
@@ -0,0 +1,47 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let state = "start";
+let flower;
+
+function preload() {
+  flower = loadImage("flowers.jpg");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  if (state === "start") {
+    startScreen();
+  }
+  if (state === "main") {
+    image(flower, 0, 0, 600, height-5);
+  }
+}
+
+function mousePressed() {
+  if (state === "start" && mouseInsideRect(400, 700, 400, 550)) {
+    state = "main";
+  }
+}
+
+function startScreen() {
+  if (mouseInsideRect) {
+    fill("gray");
+  }
+  else {
+    fill("black");
+  }
+  rect(400,400, 300,150);
+}
+
+function mouseInsideRect(left, right, top, bottom) {
+  return mouseX >= left && mouseX <= right && mouseY >= top && mouseY <= bottom;
+}
